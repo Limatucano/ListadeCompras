6977c0c3cbccaf6798ee25467e848f8f74e3745f
Matheus Lima <matheusmt31@gmail.com>
2021-02-13 01:58:17 -0300

    ajustes

--- app/src/main/java/com/example/listadecompras/MainActivity.kt
+++ app/src/main/java/com/example/listadecompras/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
